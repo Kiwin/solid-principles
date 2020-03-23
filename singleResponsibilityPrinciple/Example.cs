2e8bbc63a5ba6a7b58160430783eab50ea468533
Jacker Deluxe <Kikoro@live.dk>
2020-03-23 14:23:53 +0100

    Create Example.cs

---
+++ singleResponsibilityPrinciple/Example.cs
@@ -0,0 +1,63 @@
+/// <author> Kiwin Bendtsen Andersen </author>
+
+/// <summary>
+/// This namespace contains a bad example of the single responsability principle.
+/// </summary>
+namespace SingleResponsabilityPrinciple.BadExample
+{
+    /// <summary>
+    /// This class is has way too much responsability.
+    /// It contains the functionality of a radio and a gearbox.
+    /// 
+    /// This is not only bad because of clutter, 
+    /// but if a framework is built upon the 'Car' class, then
+    /// it will be very hard to change the behavior of the radio, or gearbox code.
+    /// </summary>
+    public abstract class Car
+    {
+        public abstract void Accelerate();
+        public abstract void Break();
+        public abstract void ChangeGearUp();
+        public abstract void ChangeGearDown();
+        public abstract void ChangeToNextRadioChannel();
+        public abstract void ChangeToPreviousRadioChannel();
+    }
+}
+
+/// <summary>
+/// This namespace contains a good example of the single responsability principle.
+/// </summary>
+namespace SingleResponsabilityPrinciple.GoodExample
+{
+    /// <summary>
+    /// This class now has the responsibility of a car.
+    /// It contains references to other classes with their own single responsability.
+    /// 
+    /// One could even argue the the 'Accelerate' method should be in a 'Motor' class.
+    /// </summary>
+    public abstract class Car
+    {
+        public Radio radio;
+        public Gearbox gearbox;
+        public abstract void Accelerate();
+        public abstract void Break();
+    }
+
+    /// <summary>
+    /// This class' single responsibility is to serve the functionality of a gearbox.
+    /// </summary>
+    public abstract class Gearbox
+    {
+        public abstract void ChangeGearUp();
+        public abstract void ChangeGearDown();
+    }
+
+    /// <summary>
+    /// This class' single responsibility is to serve the functionality of a radio.
+    /// </summary>
+    public abstract class Radio
+    {
+        public abstract void ChangeToNextRadioChannel();
+        public abstract void ChangeToPreviousRadioChannel();
+    }
+}
